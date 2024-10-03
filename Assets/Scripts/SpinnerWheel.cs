7f3c3f55d09dd47ed9bb141d3c411f27c60feaa4
chrisiansantos <jamir7n7@gmail.com>
2024-10-03 09:49:17 -0500

    Cambios de color Ruleta

--- Assets/Scripts/SpinnerWheel.cs
+++ Assets/Scripts/SpinnerWheel.cs
@@ -1,13 +1,14 @@
 using System.Collections;
+using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-using UnityEngine;
 using UnityEngine.Events;
+using System;
 
 public class SpinnerWheel : MonoBehaviour
 {
     [SerializeField]
-    int segments = 8;
+    int segments = 37; // Cambiado a 37 para una ruleta estándar europea (0-36)
     [SerializeField]
     float needleThresholdDegrees = 5f;
     [SerializeField]
@@ -21,31 +22,14 @@ public class SpinnerWheel : MonoBehaviour
     [Range(0f, 1f)]
     float durationRandomness = .5f;
     [SerializeField]
-    Image wedge;
+    UnityEngine.UI.Image wedge;
     [SerializeField]
     TextMeshProUGUI wedgeLabel;
     [SerializeField]
     [Range(0f, .01f)]
     float border;
-    [SerializeField]
-    [Range(0f, 1f)]
-    float ColorSat = 1f;
-    [SerializeField]
-    [Range(0f, 1f)]
-    float ColorVal = .5f;
     public UnityEvent OnSpin;
     public UnityEvent<int> OnResult;
-    
-    [NaughtyAttributes.Button("Draw")]
-    void DrawButtonAction()
-    {
-        Draw();
-    }
-    [NaughtyAttributes.Button("Spin!")]
-    void SpinButtonAction()
-    {
-        Spin(10f);
-    }
 
     private bool _isSpinning = false;
     public bool IsSpinning => _isSpinning;
@@ -58,7 +42,7 @@ public class SpinnerWheel : MonoBehaviour
         {
             if (_rootCanvas == null)
             {
-                _rootCanvas = GetComponentInParent<Canvas>().rootCanvas;
+                _rootCanvas = GetComponentInParent<Canvas>();
             }
             return _rootCanvas;
         }
@@ -74,11 +58,11 @@ public class SpinnerWheel : MonoBehaviour
         }
     }
 
-    private void Start() 
+    private void Start()
     {
-        Draw();    
+        Draw();
     }
-    private SpinnerWheelInputHandler spinCollider
+    private SpinnerWheelInputHandler SpinCollider
     {
         get
         {
@@ -89,55 +73,50 @@ public class SpinnerWheel : MonoBehaviour
             return _spinCollider;
         }
     }
-    private float offset => Mathf.DeltaAngle(0f, 360f / segments) / 2f;
+    private float Offset => Mathf.DeltaAngle(0f, 360f / segments) / 2f;
+
     public void Draw()
     {
         wedge.gameObject.SetActive(false);
         wedgeLabel.gameObject.SetActive(false);
-        spinCollider.transform.localEulerAngles = Vector3.zero;
+        SpinCollider.transform.localEulerAngles = Vector3.zero;
         for (int i = wedge.transform.parent.childCount - 1; i >= 0; i--)
         {
             Transform child = wedge.transform.parent.GetChild(i);
             if (child.gameObject != wedge.gameObject && child.gameObject != wedgeLabel.gameObject)
             {
-#if UNITY_EDITOR
-                UnityEditor.Undo.DestroyObjectImmediate(child.gameObject);
-#else
                 Destroy(child.gameObject);
-#endif
             }
         }
         for (int i = 0; i < segments; i++)
         {
             float borderAngle = Mathf.Lerp(0f, 180f, border);
             float angle = ((360f / segments) * i) + borderAngle;
-            Color color = Color.HSVToRGB(Mathf.InverseLerp(0, segments, i), ColorSat, ColorVal);
-            Image thisWedge = Instantiate(wedge, wedge.transform.parent);
+            Color color = (i % 2 == 0 && i != 0) ? Color.red : Color.black; // El 0 se queda negro
+            UnityEngine.UI.Image thisWedge = Instantiate(wedge, wedge.transform.parent);
             TextMeshProUGUI thisLabel = Instantiate(wedgeLabel, wedgeLabel.transform.parent);
             thisLabel.transform.position = wedgeLabel.transform.position;
-            thisWedge.transform.LocalEulerWith(z: offset - borderAngle);
+            thisWedge.transform.localEulerAngles = new Vector3(0, 0, Offset - borderAngle);
             thisWedge.color = color;
             thisWedge.fillAmount = (1f / segments) - border;
             thisWedge.transform.localPosition = wedge.transform.localPosition;
             thisWedge.gameObject.SetActive(true);
-            thisLabel.SetText((i + 1).ToString());
+            thisLabel.SetText(i.ToString());
             thisLabel.transform.SetParent(thisWedge.transform);
             thisLabel.gameObject.SetActive(true);
-            thisWedge.transform.LocalEulerWith(z: offset - angle);
-#if UNITY_EDITOR
-            UnityEditor.Undo.RegisterCreatedObjectUndo(thisWedge.gameObject, "Create Spinner Graphic");
-#endif
+            thisWedge.transform.localEulerAngles = new Vector3(0, 0, Offset - angle);
         }
     }
 
-    private float radius
+    private float Radius
     {
         get
         {
-            RectTransform spinner = spinCollider.GetComponent<RectTransform>();
+            RectTransform spinner = SpinCollider.GetComponent<RectTransform>();
             return (spinner.rect.width * .5f) * RootCanvas.scaleFactor;
         }
     }
+
     public void Spin(float spinStrength)
     {
         if (_isSpinning) return;
@@ -148,40 +127,35 @@ public class SpinnerWheel : MonoBehaviour
         if (_isSpinning) return;
         StartCoroutine(SpinRoutine(spinStrength, true));
     }
-#if UNITY_EDITOR
-    Vector3 resultPos = Vector3.zero;
-#endif
+
     private IEnumerator SpinRoutine(float strength, bool startWithStrength = false)
     {
         _isSpinning = true;
-        if (OnSpin != null) OnSpin.Invoke();
+        OnSpin?.Invoke();
         float posRandom = Mathf.Abs(durationRandomness);
-        float duration = this.duration + Random.Range(posRandom * -1f, posRandom);
+        float spinDuration = duration + UnityEngine.Random.Range(posRandom * -1f, posRandom);
         float elapsed = 0f;
-        Transform spinner = spinCollider.transform;
-        while (elapsed < duration)
+        Transform spinner = SpinCollider.transform;
+        while (elapsed < spinDuration)
         {
             float currentZ = spinner.localEulerAngles.z;
-            float curveVal = (startWithStrength) ?
-                continueCurve.Evaluate(Mathf.InverseLerp(0f, duration, elapsed)) :
-                curve.Evaluate(Mathf.InverseLerp(0f, duration, elapsed));
-
-            spinner.LocalEulerWith(z: currentZ - (curveVal * strength));
+            float curveVal = startWithStrength ? continueCurve.Evaluate(Mathf.InverseLerp(0f, spinDuration, elapsed)) : curve.Evaluate(Mathf.InverseLerp(0f, spinDuration, elapsed));
+            spinner.localEulerAngles = new Vector3(0, 0, currentZ - (curveVal * strength));
             elapsed += Time.deltaTime;
             yield return new WaitForEndOfFrame();
         }
+
         Vector3[] positions = new Vector3[segments];
         for (int i = 0; i < segments; i++)
         {
-            float angle = (360f / segments) * i - spinner.localEulerAngles.z + offset;
-            positions[i] = spinner.position + new Vector3(Mathf.Sin(Mathf.Deg2Rad * angle), Mathf.Cos(Mathf.Deg2Rad * angle), 0f).normalized * radius;
+            float angle = (360f / segments) * i - spinner.localEulerAngles.z + Offset;
+            positions[i] = spinner.position + new Vector3(Mathf.Sin(Mathf.Deg2Rad * angle), Mathf.Cos(Mathf.Deg2Rad * angle), 0f).normalized * Radius;
         }
-        Vector3 top = spinner.position + Vector3.up * radius;
+
+        Vector3 top = spinner.position + Vector3.up * Radius;
         int nearestIndex = GetNearestPoint(top, positions);
         int result = nearestIndex;
-#if UNITY_EDITOR
-        resultPos = positions[nearestIndex];
-#endif
+
         if (top.x > positions[nearestIndex].x)
         {
             result = (result + 1) % segments;
@@ -197,19 +171,16 @@ public class SpinnerWheel : MonoBehaviour
                     adjustmentElapsed += Time.deltaTime;
                     float scrub = Mathf.InverseLerp(0f, adjustmentTime, adjustmentElapsed);
                     float thisAngle = Mathf.LerpAngle(startAngle, targetAngle, scrub);
-                    spinner.LocalEulerWith(z: thisAngle);
+                    spinner.localEulerAngles = new Vector3(0, 0, thisAngle);
                     yield return new WaitForEndOfFrame();
                 }
             }
         }
 
-        if (OnResult != null)
-        {
-            OnResult.Invoke(result);
-        }
+        OnResult?.Invoke(result);
         _isSpinning = false;
     }
-    
+
     private int GetNearestPoint(Vector3 reference, Vector3[] targets)
     {
         int nearestIndex = -1;
@@ -225,24 +196,4 @@ public class SpinnerWheel : MonoBehaviour
         }
         return nearestIndex;
     }
-
-#if UNITY_EDITOR
-    private void OnDrawGizmosSelected()
-    {
-        Transform spinTransform = spinCollider.transform;
-        Gizmos.DrawCube(resultPos, Vector3.one * 30f);
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(spinTransform.position, radius);
-        for (int i = 0; i < segments; i++)
-        {
-            float angle = (360f / segments) * i - spinTransform.localEulerAngles.z + offset;
-         
-            Gizmos.color = Color.HSVToRGB(Mathf.InverseLerp(0, segments, i), 1f, 1f);
-            Vector3 dir = new Vector3(Mathf.Sin(Mathf.Deg2Rad * angle), Mathf.Cos(Mathf.Deg2Rad * angle), 0f);
-            Vector3 p1 = spinTransform.position;
-            Vector3 p2 = spinTransform.position + (dir * radius);
-            Gizmos.DrawLine(p1, p2);
-        }
-    }
-#endif
 }
